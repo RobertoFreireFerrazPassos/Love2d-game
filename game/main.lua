91d992a1294f275d271ae52311296ddcbbfce5d3
roberto freire <robertofreireferrazpassos@gmail.com>
2024-12-31 15:52:18 -0300

    place fonts in folder

--- game/main.lua
+++ game/main.lua
@@ -51,7 +51,7 @@ end
 function love.load()
     love.window.setMode(640, 480, { resizable = false })
     love.graphics.setBackgroundColor(0.1, 0.1, 0.1)
-    pixelFont = love.graphics.newFont("slkscreb.ttf", 16)
+    pixelFont = love.graphics.newFont("fonts/slkscreb.ttf", 16)
     love.graphics.setFont(pixelFont)
     loadHighScore()
     resetGame()
